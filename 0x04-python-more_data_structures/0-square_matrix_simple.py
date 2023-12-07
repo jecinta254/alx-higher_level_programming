057960b4cc86cbc5923e0a76a038489798c6e845
jecinta254 <njokijecinta254.com>
2023-12-06 21:38:39 -0500

    python day 5

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     str(matrix)
-    new_matrix = [[column ** 2 for column in row]for row in matrix]
-    return new_matrix
+    our_matrix = [[column ** 2 for column in row]for row in matrix]
+    return our_matrix
